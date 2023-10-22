fa0409d575474ba8c74143bcff2fa20b7045a35f
gab9244 <gab9244@gmail.com>
2023-10-22 20:54:29 -0300

    ongoing

---
+++ RockPaperS/src/componentes/Main.tsx
@@ -0,0 +1,19 @@
+
+function Main(){
+    return(
+        <div className="Main"> 
+        <img src="/rock-paper-scissors-master/images/bg-triangle.svg" alt="A triangle"  />
+        <div className="paperDiv">
+            <img src="/rock-paper-scissors-master/images/icon-paper.svg" alt="Paper symbol" className="paper"/>
+            </div>
+        <div className="scissorsDiv">
+            <img src="/rock-paper-scissors-master/images/icon-scissors.svg" alt="Scissors symbol" className="scissors"/>
+        </div>
+        <div className="rockDiv">
+            <img src="/rock-paper-scissors-master/images/icon-rock.svg" alt="Rock symbol" className="rock"/>
+        </div>
+        </div>
+    )
+}
+
+export default Main
